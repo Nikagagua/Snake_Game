0e28bf25fe1e64383c8d0caf9cbfcb0ca8bd797b
Nika Gagua <nikagagua@live.com>
2023-03-19 19:15:20 +0400

    added data.txt file to store high score

--- scoreboard.py
+++ scoreboard.py
@@ -4,11 +4,16 @@ ALIGNMENT = "center"
 FONT = ("Courier", 18, "bold")
 
 
+def get_high_score():
+    with open("data.txt", mode='r') as data:
+        return int(data.readline())
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = get_high_score()
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -21,11 +26,12 @@ class Scoreboard(Turtle):
         self.write_score()
 
     def write_score(self):
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}    High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.update_score()
-
